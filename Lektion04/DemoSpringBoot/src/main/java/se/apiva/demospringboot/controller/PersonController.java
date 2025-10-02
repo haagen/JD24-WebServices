7b1f4b134b51ebcb42e6a62043ce79b0cd0ff2c7
Martin Haagen <martin@mbh.se>
2025-10-02 14:17:06 +0200

    Lektion 5- repository

--- Lektion04/DemoSpringBoot/src/main/java/se/apiva/demospringboot/controller/PersonController.java
+++ Lektion04/DemoSpringBoot/src/main/java/se/apiva/demospringboot/controller/PersonController.java
@@ -1,53 +1,79 @@
 package se.apiva.demospringboot.controller;
 
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.graphql.GraphQlProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import se.apiva.demospringboot.model.Person;
+import se.apiva.demospringboot.service.PersonService;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/api/person")
 public class PersonController {
 
+    @Autowired
+    private PersonService service;
+
     @GetMapping
-    public List<Person> list() {
-        return List.of(new Person(1, "Martin", "123", "martin@mbj"));
+    public List<Person> list(
+            @RequestParam(required = false) String name
+    ) {
+
+        if (name != null && name.length() > 0) {
+            return service.listByName(name);
+        }
+
+        return service.listAll();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Person> get(@PathVariable Integer id) {
-
-        if(id == 5) {
-            return new ResponseEntity<Person>(
-                    new Person(5, "Lotta", "7654", "lotta@lotta"),
-                    HttpStatus.OK
-            );
+        try {
+            Person person = service.get(id);
+            return new ResponseEntity<Person>(person, HttpStatus.OK);
+        }  catch (Exception e) {
+            return new ResponseEntity<Person>(HttpStatus.NOT_FOUND);
         }
-
-        return new ResponseEntity<Person>(HttpStatus.NOT_FOUND);
     }
 
     @PostMapping
     public ResponseEntity<?> create(@RequestBody Person person) {
-        if(person.getId() == 5) {
-            return new ResponseEntity<>(HttpStatus.CREATED);
-        } else {
-            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        try {
+            service.save(person);
+        } catch(Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<?> update(@PathVariable Integer id, @RequestBody Person person) {
-        return new ResponseEntity<>(HttpStatus.ALREADY_REPORTED);
+
+        try {
+            service.update(id, person);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch(EntityNotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<?> delete(@PathVariable Integer id) {
-        System.out.println("We removed " + id);
-        return new ResponseEntity<>(HttpStatus.I_AM_A_TEAPOT);
+        try {
+            service.delete(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch(NoSuchElementException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
 }
---
+++ Lektion04/DemoSpringBoot/src/main/java/se/apiva/demospringboot/service/PersonService.java
@@ -0,0 +1,49 @@
+package se.apiva.demospringboot.service;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import se.apiva.demospringboot.model.Person;
+import se.apiva.demospringboot.repository.PersonRepository;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Service
+@Transactional
+public class PersonService {
+
+    @Autowired
+    private PersonRepository repo;
+
+    public List<Person> listAll() {
+        return repo.findAll();
+    }
+
+    public List<Person> listByName(String name) {
+        return repo.findByName(name);
+    }
+
+    public Person get(Integer id) throws NoSuchElementException {
+        return repo.findById(id).orElseThrow();
+    }
+
+    public void save(Person person) {
+        repo.save(person);
+    }
+
+    public void update(Integer id, Person person) {
+        Person existingPerson = get(id);    // throws exception if not found
+        if(!person.getId().equals(id)){
+            throw new EntityNotFoundException();
+        }
+        save(person);
+    }
+
+    public void delete(Integer id) {
+        Person existingPerson = get(id);
+        repo.deleteById(id);
+    }
+
+}
